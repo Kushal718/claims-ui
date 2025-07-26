6d04697cbcc71ede7244b2554dc0551d2ad0658b
Kushal Patil <kushalpatil718@gmail.com>
2025-07-26 15:58:04 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import CssBaseline from '@mui/material/CssBaseline'; // 1. Import this
+import CssBaseline from '@mui/material/CssBaseline';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* 2. Add this component */}
+    { }
     <CssBaseline />
     <App />
   </React.StrictMode>
